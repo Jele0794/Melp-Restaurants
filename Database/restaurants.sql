5118c07abc574b8d1f9e4f013dc54f5f88f303bf
Edmundo López <jlopeze@intellego.com.mx>
2018-05-10 19:14:53 -0500

    melp - Initialize project directory structure.

---
+++ Database/restaurants.sql
@@ -0,0 +1,12 @@
+Restaurants (
+id TEXT PRIMARY KEY, -- Unique Identifier of Restaurant
+            rating INTEGER, -- Number between 0 and 4
+            name TEXT, -- Name of the restaurant
+            site TEXT, -- Url of the restaurant
+            email TEXT,
+            phone TEXT,
+            street TEXT,
+            city TEXT,
+            state TEXT,
+            lat FLOAT, -- Latitude
+            lng FLOAT) -- Longitude
